8128a450be4c8b51a60c853779fa67eafae716ab
Robby Widyahartono <robby.widyahartono@gmail.com>
2023-06-15 20:43:54 +0700

    feature: add new content

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,10 +16,32 @@ module.exports = {
           '75%, 80%': { width: '80ch' },
           '85%, 90%': { width: '90ch' },
           '95%': { width: '100ch' },
-        }
+        },
+        'fade-in-left': {
+          '0%': {
+              opacity: '0',
+              transform: 'translateX(500px)'
+          },
+          '100%': {
+              opacity: '1',
+              transform: 'translateX(0)'
+          },
+        },
+        'fade-in-down': {
+          '0%': {
+              opacity: '0',
+              transform: 'translateY(-40px)'
+          },
+          '100%': {
+              opacity: '1',
+              transform: 'translateX(0)'
+          },
+        },
       },
       animation: {
         type: 'type 1.8s ease-out .8s 1 normal both',
+        'fade-in-left': 'fade-in-left 0.5s ease-in',
+        'fade-in-down': 'fade-in-down 0.5s ease-in',
       },
       fontFamily: {
         'courier-prime': ['Courier Prime', 'monospace']
